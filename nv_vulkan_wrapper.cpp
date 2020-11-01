e6243dac7642353e5c95bbdaae92d69d4857cbb1
Felix Dörre <felix@dogcraft.de>
2020-11-01 23:49:57 +0100

    cleanup the nv-via-glx hack

--- nv_vulkan_wrapper.cpp
+++ nv_vulkan_wrapper.cpp
@@ -5,6 +5,7 @@
 #include <GL/glx.h>
 
 #include <string>
+#include <memory>
 #include <iostream>
 
 extern "C" VKAPI_ATTR VkResult VKAPI_CALL vk_icdNegotiateLoaderICDInterfaceVersion(uint32_t* pSupportedVersion);
@@ -25,9 +26,68 @@ typedef GLXContext (*GLXCREATECONTEXTATTRIBSARBPROC)(Display*, GLXFBConfig, GLXC
 
 typedef void *(*getproc )(const char* name);
 
+class BBGLXContext {
+public:
+  Display *dpy;
+  Window win;
+  GLXContext ctx;
+public:
+  BBGLXContext() = default;
+  BBGLXContext(const BBGLXContext &copy) = delete;
+  BBGLXContext(const char* display){
+    dpy = XOpenDisplay(display);
+    if(!dpy){
+      std::cout << "Can't open bumblebee display.\n";
+      return;
+    }
+    
+    int nelements;
+    GLXFBConfig *fbc = glXChooseFBConfig(dpy, DefaultScreen(dpy), 0, &nelements);
+    static int attributeList[] = { GLX_RGBA, GLX_DOUBLEBUFFER, GLX_RED_SIZE, 1, GLX_GREEN_SIZE, 1, GLX_BLUE_SIZE, 1, None };
+    XVisualInfo *vi = glXChooseVisual(dpy, DefaultScreen(dpy),attributeList);
+
+    XSetWindowAttributes swa;
+    swa.colormap = XCreateColormap(dpy, RootWindow(dpy, vi->screen), vi->visual, AllocNone);
+    swa.border_pixel = 0;
+    swa.event_mask = StructureNotifyMask;
+    win = XCreateWindow(dpy, RootWindow(dpy, vi->screen), 0, 0, 100, 100, 0, vi->depth, InputOutput, vi->visual, CWBorderPixel|CWColormap|CWEventMask, &swa);
+    XFree(vi);
+
+    GLXCREATECONTEXTATTRIBSARBPROC pfn_glXCreateContextAttribsARB = (GLXCREATECONTEXTATTRIBSARBPROC) glXGetProcAddress((const GLubyte*)"glXCreateContextAttribsARB");
+
+    int attribs[] = {
+      GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
+      GLX_CONTEXT_MINOR_VERSION_ARB, 0,
+      0};
+    ctx = pfn_glXCreateContextAttribsARB(dpy, *fbc, 0, true, attribs);
+    XFree(fbc);
+  }
+  ~BBGLXContext() {
+    if(dpy != nullptr) {
+      glXDestroyContext(dpy, ctx); 
+      XDestroyWindow(dpy, win);
+      XCloseDisplay(dpy);
+    }
+  }
+  BBGLXContext& operator=(BBGLXContext&& other) noexcept {
+    dpy = other.dpy;
+    win = other.win;
+    ctx = other.ctx;
+    other.dpy = nullptr;
+    return *this;
+  }
+  bool isValid(){
+    return dpy != nullptr;
+  }
+  void makeCurrent(){
+    glXMakeCurrent (dpy, win, ctx);
+  }
+};
+#include <thread>
 class StaticInitialize {
   void *nvDriver;
   void *glLibGL;
+  BBGLXContext glx;
 public:
   VKAPI_ATTR PFN_vkVoidFunction (*instanceProcAddr) (VkInstance instance,
                                                const char* pName);
@@ -40,49 +100,24 @@ public:
     // This ensures that ld.so will find this libGL before the Nvidia one, when
     // again asked to load libGL.
     glLibGL = dlopen("libGL.so.1", RTLD_GLOBAL | RTLD_NOW);
+    glx = BBGLXContext(NV_BUMBLEBEE_DISPLAY);
+    if(!glx.isValid()){
+	return;
+    }
 
-	Display *dpy = XOpenDisplay(NV_BUMBLEBEE_DISPLAY);
-
-	int nelements;
-	GLXFBConfig *fbc = glXChooseFBConfig(dpy, DefaultScreen(dpy), 0, &nelements);
-	static int attributeList[] = { GLX_RGBA, GLX_DOUBLEBUFFER, GLX_RED_SIZE, 1, GLX_GREEN_SIZE, 1, GLX_BLUE_SIZE, 1, None };
-	XVisualInfo *vi = glXChooseVisual(dpy, DefaultScreen(dpy),attributeList);
-
-	XSetWindowAttributes swa;
-	swa.colormap = XCreateColormap(dpy, RootWindow(dpy, vi->screen), vi->visual, AllocNone);
-	swa.border_pixel = 0;
-	swa.event_mask = StructureNotifyMask;
-	auto win = XCreateWindow(dpy, RootWindow(dpy, vi->screen), 0, 0, 100, 100, 0, vi->depth, InputOutput, vi->visual, CWBorderPixel|CWColormap|CWEventMask, &swa);
-
-	GLXCREATECONTEXTATTRIBSARBPROC pfn_glXCreateContextAttribsARB = (GLXCREATECONTEXTATTRIBSARBPROC) glXGetProcAddress((const GLubyte*)"glXCreateContextAttribsARB");
-
-	int attribs[] = {
-		GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
-		GLX_CONTEXT_MINOR_VERSION_ARB, 0,
-		0};
-
-	GLXContext ctx = pfn_glXCreateContextAttribsARB(dpy, *fbc, 0, true, attribs);
-	glXMakeCurrent (dpy, win, ctx);
-	getproc fn2 = (getproc) glXGetProcAddress((const GLubyte*) "glGetVkProcAddrNV");
-	void *glfn = fn2("vk_icdGetInstanceProcAddr");
-	std::cout << ctx << std::endl;
+    glx.makeCurrent();
+    getproc fn2 = (getproc) glXGetProcAddress((const GLubyte*) "glGetVkProcAddrNV");
+    void *glfn = fn2("vk_icdGetInstanceProcAddr");
 
     instanceProcAddr = (decltype(instanceProcAddr)) fn2("vk_icdGetInstanceProcAddr");
     phyProcAddr = (decltype(phyProcAddr)) fn2("vk_icdGetPhysicalDeviceProcAddr");
     negotiateVersion = (decltype(negotiateVersion)) fn2("vk_icdNegotiateLoaderICDInterfaceVersion");
-	glXDestroyContext(dpy, ctx); 
-	XFree(vi);
-	XFree(fbc);
-	XDestroyWindow(dpy, win);
-	XCloseDisplay(dpy);
   }
   ~StaticInitialize(){
-    if(nvDriver)
-      dlclose(nvDriver);
     dlclose(glLibGL);
   }
   bool IsInited(){
-    return nvDriver != nullptr;
+    return negotiateVersion != nullptr;
   }
 };
 
@@ -106,5 +141,6 @@ extern "C" VKAPI_ATTR VkResult VKAPI_CALL vk_icdNegotiateLoaderICDInterfaceVersi
   if (!init.IsInited()) {
     return VK_ERROR_INCOMPATIBLE_DRIVER;
   }
+  
   return init.negotiateVersion(pSupportedVersion);
 }
