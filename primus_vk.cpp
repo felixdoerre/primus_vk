6da5051bed8acaa8a38287ad67eea3c4153d27d9
Felix Dörre <felix@dogcraft.de>
2024-10-19 00:15:50 +0200

    fixup extension filtering

--- primus_vk.cpp
+++ primus_vk.cpp
@@ -1297,29 +1297,38 @@ VkResult VKAPI_CALL PrimusVK_EnumerateDeviceExtensionProperties(
 
     scoped_lock l(global_lock);
     std::unique_ptr<std::vector<VkExtensionProperties>> extensions;
-    auto res = instance_dispatch[GetKey(physicalDevice)].EnumerateDeviceExtensionProperties(physicalDevice, pLayerName, pPropertyCount, pProperties);
-    if(pProperties == nullptr && res == VK_SUCCESS) {
-      extensions = std::make_unique<std::vector<VkExtensionProperties>>(*pPropertyCount);
-      pProperties = extensions->data();
-      res = instance_dispatch[GetKey(physicalDevice)].EnumerateDeviceExtensionProperties(physicalDevice, pLayerName, pPropertyCount, pProperties);
+    uint32_t allProps = 0;
+    auto res = instance_dispatch[GetKey(physicalDevice)].EnumerateDeviceExtensionProperties(physicalDevice, pLayerName, &allProps, nullptr);
+    extensions = std::make_unique<std::vector<VkExtensionProperties>>(allProps);
+
+    if(res != VK_SUCCESS) {
+      TRACE("Early Return0?");
+      return res;
     }
+    
+    res = instance_dispatch[GetKey(physicalDevice)].EnumerateDeviceExtensionProperties(physicalDevice, pLayerName, &allProps, extensions->data());
     if(res != VK_SUCCESS) {
       return res;
     }
     // Low_latency_2 has functions configuring a swapchain-device-combination.
     // As primus_vk breaks this link, passing the extension through as-is fails.
     // Implementing the extension seems non-trivial, so for now we skip it.
-    std::string toFilter = "VK_NV_low_latency2";
-    int foundAt = -1;
-    for(int i = 0; i < *pPropertyCount; i++) {
-      if(toFilter == pProperties[i].extensionName){
-	foundAt = i;
+    auto mProps = extensions->data();
+    int outPos = 0;
+    int inPos = 0;
+    *pPropertyCount = allProps;
+    for(int i = 0; i < allProps; i++) {
+      if(std::string{"VK_NV_low_latency"} == mProps[i].extensionName ||
+	 std::string{"VK_NV_low_latency2"} == mProps[i].extensionName) {
+	if(pProperties)
+	  std::copy(&mProps[inPos], &mProps[i], &pProperties[outPos]);
+	(*pPropertyCount)--;
+	outPos += i - inPos;
+	inPos = i + 1;
       }
     }
-    if(foundAt != -1){
-      std::copy(&pProperties[foundAt+1], &pProperties[*pPropertyCount], &pProperties[foundAt]);
-      *pPropertyCount--;
-    }
+    if(pProperties)
+      std::copy(&mProps[inPos], &mProps[allProps], &pProperties[outPos]);
     return res;
   }
 
